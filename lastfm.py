5ec170657664ecbeae38682030949909bd0fa594
Jaime Ramírez <jaime.ramirez@roiback.com>
2019-07-26 09:22:24 +0200

    Added Youtube example

--- lastfm.py
+++ lastfm.py
@@ -5,12 +5,20 @@ from pprint import pprint
 
 lastfm = pylast.LastFMNetwork(
     api_key=os.environ["LASTFM_API_KEY"],
-    api_secret=os.environ["LASTFM_API_SECRET"]
+    api_secret=os.environ["LASTFM_API_SECRET"],
+    # username=os.environ["LASTFM_USERNAME"],
+    # password_hash=pylast.md5(os.environ["LASTFM_PASSWORD"]),
 )
 
 # Now you can use that object everywhere
 user = lastfm.get_user("jimmydj2000")
 print(user)
+
+print("TRack")
+track = lastfm.get_track("Vangelis", "Tears in the rain")
+
+print(track)
+print(track.get_wiki_summary())
 
 
 loved_tracks = user.get_loved_tracks(limit=5)
